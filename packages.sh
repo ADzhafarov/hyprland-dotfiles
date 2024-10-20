41f71091815415b958f9af97ff98d4db0345c510
genzonline <genzonlinew@gmail.com>
2024-10-20 19:36:27 +0400

    Added packagelist

---
+++ packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pacman -S alacritty alsa-utils amd-ucode android-tools base chromium debugedit discord docker docker-compose dolphin dunst efibootmgr fakeroot fd fish gcc git go grim grub hyprland hyprpaper jack-example-tools jq kitty linux linux-firmware luarocks make man-db man-pages neofetch neovim networkmanager npm obs-studio obsidian openssh openvpn otf-font-awesome pacman-contrib pavucontrol pipewire-alsa pipewire-pulse postgresql postman-bin pyenv python-pip python-pynvim qt5-graphicaleffects qt5-quickcontrols2 reflector ripgrep sddm slurp spotify-launcher sudo telegram-desktop terminus-font texinfo unzip waybar webcord wezterm wget wl-clipboard wofi xdg-desktop-portal-hyprland xdg-desktop-portal-wlr zip
+
+
